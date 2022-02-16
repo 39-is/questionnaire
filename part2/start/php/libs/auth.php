906594868c48913a9ad663ff1bdb7a3c922b85a6
39-is <64857572+39-is@users.noreply.github.com>
2022-02-16 17:47:04 +0900

    ユーザー登録

--- part2/start/php/libs/auth.php
+++ part2/start/php/libs/auth.php
@@ -26,4 +26,18 @@ class Auth {
             return $is_success;
     }
 
+    public static function regist($id, $pwd, $nickname){
+            $is_success = false;
+
+            $exit_user = UserQuery::fetchById($id);
+
+            if (!empty($exit_user)){
+                echo 'ユーザーが既に存在します';
+                return false;
+            }
+
+            $is_success = UserQuery::insert($id, $pwd, $nickname);
+            return $is_success;
+    }
+
 }
--- part2/start/php/views/register.php
+++ part2/start/php/views/register.php
@@ -1,2 +1,15 @@
-    <h1>Registerページ</h1>
-    <!-- <img src="<?php echo BASE_IMAGE_PATH ?>logo.svg" alt=""> -->
+<h1>Registerページ</h1>
+    <form action="<?php echo CURRENT_URI; ?>" method="POST">
+　　　　<div>
+    　id:<input type="text" name="id">
+　　　　</div>
+　　　　<div>
+    　pwd:<input type="password" name="pwd">
+　　　　</div>
+　　　　　<div>
+    　nickname:<input type="password" name="nickname">
+　　　　</div>
+　　　　<div>
+    　<input type="submit" value="ログイ-ン">
+　　　　</div>
+</form>
