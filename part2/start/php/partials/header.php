e852eeab0cd871233a3879ddf17858a53d43c924
39-is <64857572+39-is@users.noreply.github.com>
2022-02-23 02:41:06 +0900

    css3

--- part2/start/php/partials/header.php
+++ part2/start/php/partials/header.php
@@ -5,14 +5,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>みんなのアンケート</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"
-    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="//fonts.googleapis.com/css2?family=Shizuru&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
     
 </head>
 <body>
     <div id="container">
-
-    <header class="container my-2">
+        <header class="container my-2">        <!--上のヘッダー-->
         <nav class="row align-items-center py-2">
             <a href="" class="col-md d-flex align-items-center mb-3 mb-md-o">
                 <img width="50" class="mr-2" src="images/logo.svg" alt="みんなのアンケート　サイトロゴ">
@@ -25,9 +26,17 @@
 
             </div>
         </nav>
-    </header>
-    <main class="container py-3">
+
+    </header>　   <!--上のヘッダー-->
+
+    <main class="container py-3">   <!--真ん中の枠-->
+        <h1 class="sr-only">ログイン</h1>
+        <div class="mt-5">
+        <div class="text-center mb-4">
+            <image width="65" src="images/logo.svg" alt="みんなのアンケート　サイトロゴ">
+        </div>
     <div class="login-form bg-white p-4 shadow-sm mx-auto rounded">
+
     <form action="">
             <div class="form-group">
         <label for="">ユーザーID</label>
@@ -37,14 +46,27 @@
         <label for="">パスワード</label>
         <input type="password" class="form-control">
             </div>
+        <div class="d-flex align-items-center justify-content-between">
+            <div>
+            <a href="">アカウント登録</a>
+            </div>
+            <div>
+            <input type="submit" value="ログイン" class="btn btn-primary shadow-sm">
+            </div>
+        </div>
     </form>
 
     </div>
 
+        </div>
+        
 
-    </main>
 
-    <footer></footer>
+    </main> <!--真ん中の枠-->
+
+    <footer class="text-center p-3">
+        <span class="text-muted"> &copy; SEIICHI</span>
+    </footer>
     
 
     </div>
